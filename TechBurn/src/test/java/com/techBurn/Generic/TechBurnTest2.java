47f7ac973592c14cb4c9520d323bb74e7893c53f
Muzamilsyed789 <muzmilsyed789@gmail.com>
2024-04-20 12:34:31 +0530

    commiting second time

--- TechBurn/src/test/java/com/techBurn/Generic/TechBurnTest2.java
+++ TechBurn/src/test/java/com/techBurn/Generic/TechBurnTest2.java
@@ -1,5 +1,10 @@
 package com.techBurn.Generic;
 
-public class TechBurnTest2 {
+import org.testng.annotations.Test;
 
+public class TechBurnTest2 {
+@Test
+public void main1() {
+	System.out.println(" running");
+}
 }
